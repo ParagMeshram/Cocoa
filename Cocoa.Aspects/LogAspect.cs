77a12803d323901250810953fea58c3afe03759e
Parag Meshram <parag.meshram@hotmail.com>
2020-10-21 00:49:27 +0530

    Initial commit

---
+++ Cocoa.Aspects/LogAspect.cs
@@ -0,0 +1,25 @@
+﻿namespace Cocoa.Aspects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using MethodBoundaryAspect.Fody.Attributes;
+
+    public class LogAspectAttribute : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            
+        }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            
+        }
+    }
+}
